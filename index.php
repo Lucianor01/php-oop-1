95434c7f7ede958d7cefcf4fb78d1e108527adf4
Lucianor01 <lucianorosario02@gmail.com>
2023-05-31 16:47:23 +0200

    costruttore

--- index.php
+++ index.php
@@ -6,22 +6,30 @@ class Movie
     public $genere;
     public $regista;
     public $anno;
+
+    public function __construct($titolo, $genere, $regista, $anno)
+    {
+        $this->titolo = $titolo;
+        $this->genere = $genere;
+        $this->regista = $regista;
+        $this->anno = $anno;
+    }
 }
 
-$joker = new Movie();
+$joker = new Movie('Joker', 'Thriller/Drammatico', 'Todd Phillips', '2019');
 
-$joker->titolo = 'Joker';
-$joker->genere = 'Thriller/Drammatico';
-$joker->regista = 'Todd Phillips';
-$joker->anno = '2019';
+// $joker->titolo = 'Joker';
+// $joker->genere = 'Thriller/Drammatico';
+// $joker->regista = 'Todd Phillips';
+// $joker->anno = '2019';
 
 var_dump($joker);
 
-$the_Batman = new Movie();
+$the_Batman = new Movie('The Batman', 'Azione/Giallo', 'Matt Reeves', '2022');
 
-$the_Batman->titolo = 'The Batman';
-$the_Batman->genere = 'Azione/Giallo';
-$the_Batman->regista = 'Matt Reeves';
-$the_Batman->anno = '2022';
+// $the_Batman->titolo = 'The Batman';
+// $the_Batman->genere = 'Azione/Giallo';
+// $the_Batman->regista = 'Matt Reeves';
+// $the_Batman->anno = '2022';
 
 var_dump($the_Batman);
